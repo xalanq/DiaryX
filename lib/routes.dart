0fb59c1c6cf180af6c77ce5250c9c5a49878f7b2
xalanq <xalanq@gmail.com>
2025-08-03 15:22:54 +0800

    upd(widgets): remove unused widgets

--- lib/routes.dart
+++ lib/routes.dart
@@ -2,13 +2,6 @@
 class AppRoutes {
   static const String splash = '/';
   static const String home = '/home';
-  static const String capture = '/capture';
-  static const String timeline = '/timeline';
-  static const String report = '/report';
-  static const String search = '/search';
-  static const String profile = '/profile';
-  static const String momentDetail = '/moment';
-  static const String llmAnalysis = '/analysis';
 
   /// Private constructor to prevent instantiation
   AppRoutes._();
